d5da6a393606de2f4d2a9b6d3e84b896787e0c17
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-29 22:06:06 -0500

    fixing Merge Array

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 3/6/18.
  */
@@ -10,19 +14,34 @@ public class ArrayUtility<T> {
         this.t = inputArray;
     }
 
-    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        T[] temp = mergeArray(arrayToMerge);
+        int count =0;
+        for(int i = 0; i < temp.length; i++){
+            for(int j =0; j <temp.length; j++){
+                if(temp[i].equals(valueToEvaluate) && temp[j].equals(valueToEvaluate)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
         return null;
     }
 
-    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
         return null;
     }
 
-    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+    public T[] removeValue(T valueToRemove) {
         return null;
     }
 
-    public Integer[] removeValue(Integer valueToRemove) {
-        return null;
+    public T[] mergeArray(T[] arrayToMerge){
+        T[] newArr = Arrays.copyOf(t, t.length-1+arrayToMerge.length-1);
+        System.arraycopy(arrayToMerge, 0, t, t.length-1, arrayToMerge.length-1);
+        return newArr;
     }
 }
