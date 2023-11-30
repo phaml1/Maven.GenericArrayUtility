122ebd76866654896a4b5741111430147b28226f
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-29 21:35:29 -0500

    generate empty methods

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,26 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] t;
+
+    public ArrayUtility(T[] inputArray) {
+        this.t = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+        return null;
+    }
+
+    public Integer getMostCommonFromMerge(Integer[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(Integer valueToEvaluate) {
+        return null;
+    }
+
+    public Integer[] removeValue(Integer valueToRemove) {
+        return null;
+    }
 }
